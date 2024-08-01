9733a655f28a08e64e8f088117a9a0b10addb03a
gopicherukumalli <gopi123ch@gmail.com>
2024-08-01 18:32:01 +0530

    chngs

--- src/test/java/flows/FlyAdealCacheFlow.java
+++ src/test/java/flows/FlyAdealCacheFlow.java
@@ -97,7 +97,7 @@ public class FlyAdealCacheFlow{
 				options.addArguments("--headless");
 				driver = new FirefoxDriver(options);
 				driver.manage().window().maximize();
-				driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 				driver.manage().deleteAllCookies();
 				driver.get(flyAdealApiUrl);
 				new BaseClass(driver);
